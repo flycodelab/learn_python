9052d6eeb8965d149dd2b77ec621628a838e016d
flycodelab <flycodelab@gmail.com>
2019-02-13 00:22:55 +0900

    chapter source code #2

---
+++ book1_brain/09/212_overriding.py
@@ -0,0 +1,16 @@
+class A:
+    def method(self):
+        print("A")
+
+class B(A):
+    def method(self):
+        print("B")
+
+class C(A):
+    def method(self):
+        print("C")
+
+A().method()
+B().method()
+C().method()
+
